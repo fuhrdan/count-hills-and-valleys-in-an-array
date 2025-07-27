8d2f7650ec2a3fa1fe4457029ad3e05939316869
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-07-27 08:41:55 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//*****************************************************************************
+//** 2210. Count Hills and Valleys in an Array                      leetcode **
+//*****************************************************************************
+
+int countHillValley(int* nums, int numsSize) {
+    int i = 1;
+    int ans = 0;
+    while (i < numsSize) {
+        int a = i - 1;
+        while (i + 1 < numsSize && nums[i] == nums[i + 1]) i++;
+        if (i == numsSize - 1) break;
+        int b = i + 1;
+        if ((nums[b] < nums[i] && nums[i] > nums[a]) || 
+            (nums[b] > nums[i] && nums[i] < nums[a])) {
+                ans++;
+            }
+        i = b;
+        }
+    return ans;    
+}
